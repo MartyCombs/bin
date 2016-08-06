c2f9bef1519970ce67a19ef13b78c9e79eaa2791
me <marty@tipsy.local>
2016-08-06 11:27:19 -0700

    Update to print keywords

--- keywords.py
+++ keywords.py
@@ -1,68 +1,83 @@
 #!/usr/bin/env python
 
-import argparse, sys, os, subprocess, shlex, re
+import argparse, sys, os, shlex, re
+import subprocess
 
-parser = argparse.ArgumentParser(description='Update keywords on an image or movie.')
-parser.add_argument('files', metavar='FILENAME', type=str, nargs='+', help='List of image or movie files to manipulate.')
-parser.add_argument('--debug', action='store_true', help='Reflect debug information.')
-parser.add_argument('--keep', action='store_true', help='Keep originals from exiftool')
-parser.add_argument('-a', '--addkey', nargs='?', action='append', type=str, help='List of keywords to add to EXIF data.')
-parser.add_argument('-r', '--removekey', nargs='?', action='append', type=str, help='List of keywords to remove from EXIF data.')
-parser.add_argument('-s', '--setkey', action='store', type=str, help='List of keywords to specifically set EXIF data.')
-parser.add_argument('-c', '--confirm', action='store_true', help='Confirm writing of EXIF data.')
-args = parser.parse_args()
-
-#-----------------------------------------------------------------------------#
-# Sanity tests
-filenames=[]
-files_skipped=[]
-for filename in args.files:
-    if not os.path.exists(filename):
-        files_skipped.append(filename)
-    else:
-        checkexifdata = subprocess.check_output(["exiftool", "-Keywords", filename])
-        if checkexifdata is None:
-            files_skipped.append(filename)
-        else:
-            filenames.append(filename)
 
 class Keyword:
-    'Add keywords to an image file'
-    exifdata={}
-    def __init__(self, filelist):
-        self.files = filelist
-    def ReadExif(self):
-        for filename in self.files:
-            try:
-                exiftool_output = subprocess.check_output(["exiftool", "-Keywords", filename])
-                kwords = re.search(r'\: (.*)$', exiftool_output).group(1)
-                Keyword.exifdata[filename] = [k.strip() for k in kwords.split(',')]
-            except AttributeError:
-                Keyword.exifdata[filename] = []
-    def PrintKeywords(self, imagefile):
-        print 'File {}'.format(imagefile)
-        print '     {}'.format(Keyword.exifdata[imagefile])
-    def CleanList(self, imagefile):
-        Keyword.exifdata[imagefile] = list(set(Keyword.exifdata[imagefile]))
-        Keyword.exifdata[imagefile].sort()
-    def AddKeywords(self, imagefile, keystoadd):
-        keywords_existing = Keyword.exifdata[imagefile]
+    'Manage keywords for files which can have them.'
+    def __init__(self, files=None, actions=None):
+        '''
+Parameters
+    files          List of files to examine and/or alter.
+    actions        List of actions to take.
+        '''
+        self.files = []
+        self.files_skipped = []
+        self.keywords_before = {}
+        self.keywords = {}
+        self.maxlength = len('Filename')
+        # If every element in the list of actions is 'None', no action should be taken.
+        self.noaction = True
+        if type(actions) is list:
+            for a in actions:
+                if type(a) is list:
+                    self.noaction = False
+        for f in files:
+            if not os.path.exists(f):
+                sys.stderr.write('File not found "{}"'.format(f))
+                self.files_skipped.append(f)
+                continue
+            else:
+                checkexifdata = self.read_exif_data(filename=f)
+                if checkexifdata['error'] is not None:
+                    sys.stderr.write('Error in exiftool for file "{}" : {}'.format(f, checkexifdata['error']))
+                    self.files_skipped.append(f)
+                else:
+                    self.files.append(f)
+                    self.keywords[f] = checkexifdata['keywords']
+                    self.keywords_before[f] = self.keywords[f].sort()
+        # Set the buffer to the longest filename found.
+        for ef in self.keywords.keys():
+            if self.maxlength < len(ef):
+                self.maxlength = len(ef)
+    def read_exif_data(self, filename=None):
+        keywords = []
+        error = None
+        cmd = ['exiftool', '-Keywords', filename]
+        try:
+            exiftool_output = subprocess.check_output(cmd, stderr=subprocess.STDOUT)
+            kwords = re.search(r'\: (.*)$', exiftool_output).group(1)
+            keywords = [k.strip() for k in kwords.split(',')]
+        except AttributeError:
+            self.keywords[filename] = []
+        except subprocess.CalledProcessError as e:
+            error = e.output
+        return { 'keywords' : keywords,
+                 'error' : error }
+    def print_keywords(self, filename):
+        print '{:<}'.format(self.keywords[filename])
+    def clean_list(self, filename):
+        self.keywords[filename] = list(set(self.keywords[filename]))
+        self.keywords[filename].sort()
+    def add_keywords(self, filename, keystoadd):
+        keywords_existing = self.keywords[filename]
         for keytoadd in keystoadd:
             if not keytoadd in keywords_existing:
-                Keyword.exifdata[imagefile].append(keytoadd)
-    def RemoveKeywords(self, imagefile, keystoremove):
-        keywords_existing = Keyword.exifdata[imagefile]
+                self.keywords[filename].append(keytoadd)
+    def remove_keywords(self, filename, keystoremove):
+        keywords_existing = self.keywords[filename]
         for keytoremove in keystoremove:
             if keytoremove in keywords_existing:
-                Keyword.exifdata[imagefile].remove(keytoremove)
-    def SetKeywords(self, imagefile, keywords):
-        Keyword.exifdata[imagefile] = keywords.split()
-    def WriteKeywords(self, imagefile):
-        kwords = ', '.join(Keyword.exifdata[imagefile])
-        cmd='exiftool -Keywords="{}" {}'.format(kwords,imagefile)
+                self.keywords[filename].remove(keytoremove)
+    def set_keywords(self, filename, keywords):
+        self.keywords[filename] = keywords.split()
+    def write_keywords(self, filename):
+        kwords = ', '.join(self.keywords[filename])
+        cmd='exiftool -overwrite_original_in_place -Keywords="{}" {}'.format(kwords,filename)
         args = shlex.split(cmd)
         subprocess.check_output(args)
-    def ConfirmPrompt(self, prompt=None, resp=False):
+    def confirm_prompt(self, prompt=None, resp=False):
         if prompt is None:
             prompt = 'Confirm'
         if resp:
@@ -82,26 +97,52 @@ class Keyword:
                 return False
 
 
-#-----------------------------------------------------------------------------#
-kw = Keyword(filenames)
-kw.ReadExif()
-for filename in filenames:
-    if kw.debug:
-        print 'File : {}'.format(filename)
-        print '    BEFORE : {}'.format(kw.exifdata[filename])
-    if args.addkey:
-        kw.AddKeywords(imagefile = filename, keystoadd = args.addkey)
-    if args.removekey:
-        kw.RemoveKeywords(imagefile = filename, keystoremove = args.removekey)
-    if args.setkey:
-        kw.SetKeywords(imagefile = filename, keywords = args.setkey)
+def main():
+    parser = argparse.ArgumentParser(description='Update keywords on an image or movie.')
+    parser.add_argument('files', metavar='FILENAME', type=str, nargs='+', default=None, help='List of image or movie files to manipulate.')
+    parser.add_argument('--debug', action='store_true', default=False, help='Reflect debug information.')
+    parser.add_argument('--noop', action='store_true', default=False, help='Do not do anything.')
+    parser.add_argument('-q', '--quiet', action='store_true', default=False, help='Verbose output.')
+    parser.add_argument('--keep', action='store_true', default=False, help='Keep originals from exiftool')
+    parser.add_argument('-a', '--addkey', nargs='?', action='append', type=str, default=None, help='List of keywords to add to EXIF data.')
+    parser.add_argument('-r', '--removekey', nargs='?', action='append', type=str, default=None, help='List of keywords to remove from EXIF data.')
+    parser.add_argument('-s', '--setkey', action='store', type=str, default=None, help='List of keywords to specifically set EXIF data.')
+    parser.add_argument('-c', '--confirm', action='store_true', default=False, help='Confirm writing of EXIF data.')
+    args = parser.parse_args()
 
-    # Order the keywords alphabetically before writing them.
-    kw.CleanList(imagefile = filename)
-    if kw.debug:
-        print '    AFTER  : {}'.format(kw.exifdata[filename])
+    kw = Keyword(files=args.files, actions=[args.addkey, args.removekey, args.setkey])
+    if len(kw.files) == 0:
+        sys.stderr.write('No files to update.\n')
+        sys.exit(0)
+    for f in kw.files:
+        if args.quiet == False and kw.noaction == False:
+            sys.stderr.write('File : {}\n'.format(f))
+            sys.stderr.write('    BEFORE : {}\n'.format(kw.keywords[f]))
+        if args.addkey:
+            kw.add_keywords(filename=f, keystoadd=args.addkey)
+        if args.removekey:
+            kw.remove_keywords(filename=f, keystoremove=args.removekey)
+        if args.setkey:
+            kw.set_keywords(filename=f, keywords=args.setkey)
+
+        # Order the keywords alphabetically before writing them.
+        kw.clean_list(filename=f)
+        if args.quiet == False and kw.noaction == False:
+            sys.stderr.write('    AFTER  : {}\n'.format(kw.keywords[f]))
+    if args.noop:
+        sys.exit(0)
     if args.confirm is True:
-        if kw.ConfirmPrompt(prompt='Write metadata?', resp=False) is True:
-            kw.WriteKeywords(filename)
-    else:
-        kw.WriteKeywords(filename)
+        if kw.confirm_prompt(prompt='Write metadata?', resp=False) is False:
+            sys.exit(0)
+    if kw.noaction is True:
+        print '{fname:{chars}s}    {kwords:<}'.format(fname='Filename', chars=kw.maxlength, kwords='Keywords')
+        print '{fname:{chars}s}    {kwords:<}'.format(fname='--------', chars=kw.maxlength, kwords='--------')
+    for f in kw.files:
+        if kw.noaction is True:
+            print '{fname:{chars}s}    {kwords:<}'.format(fname=f, chars=kw.maxlength, kwords=kw.keywords[f])
+            if kw.keywords[f] != kw.keywords_before[f]:
+                kw.write_keywords(filename=f)
+
+
+if __name__ == "__main__":
+    main()
