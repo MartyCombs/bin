1bcff05c9c5d457dfb859265ce1eb18ebc71d71c
me <marty@tipsy.local>
2016-08-07 09:13:06 -0700

    Code clean up.  Include full path in renaming files.

--- pic-number.py
+++ pic-number.py
@@ -14,32 +14,44 @@ class NumberPics:
         self.noop = noop
         self.prepend = prepend
         self.nameformat = nameformat
+        self.base_directory = {}
         self.filenames_to_change = []
+        self.filenames_skipped = []
         self.filenames_already_matching = []
         self.filename_format = re.compile(r'^(\S+)\-\d+\.([A-Za-z0-9]{3,4})\Z')
         self.total_files = 0
+        self.num_digits = 0
         self.files = {}
 
 
-
     def build_list(self, filelist=None):
+        to_skip = []
+        already_matching = []
+        to_change = []
+        path = {}
         for f in sorted(filelist):
+            basename = os.path.basename(f)
+            path[basename] = os.path.dirname(f)
             # Skip any files which begin with a dot as they may be thumbnails.
-            if f.startswith('.'):
-                print 'WARN: Will not rename dot-files.  Skipping "{}"'.format(f)
+            if basename.startswith('.'):
+                to_skip.append(basename)
                 continue
-            if fnmatch.fnmatch(f, self.nameformat + '*'):
-                self.filenames_already_matching.append(f)
+            if fnmatch.fnmatch(basename, self.nameformat + '*'):
+                already_matching.append(basename)
             else:
-                self.filenames_to_change.append(f)
-        if self.debug:
-            print 'DEBUG: Found {} files matching name "{}" and {} files to update.'.format(len(self.filenames_already_matching), self.nameformat, len(self.filenames_to_change))
-
+                to_change.append(basename)
+        self.filenames_to_change = to_change
+        self.filenames_already_matching = already_matching
+        self.filenames_skipped = to_skip
+        self.base_directory = path
+        self.total_files = len(self.filenames_already_matching) + len(self.filenames_to_change)
+        self.num_digits = len(str(self.total_files))
 
 
     def replace_files(self, files=None, replace_with=None, index=None, digits=0):
         if index < 1: return index
         for f in sorted(files, reverse=True):
+            oldname = f
             if replace_with:
                 replace = re.match(r'^(\S+)\.([A-Za-z0-9]{3,4})\Z', f)
                 newname = replace_with + '-' + str(index).zfill(digits) + '.' + replace.group(2).lower()
@@ -50,24 +62,21 @@ class NumberPics:
                 else:
                     keep = re.match(r'^(\S+)\.([A-Za-z0-9]{3,4})\Z', f)
                     newname = keep.group(1) + '-' + str(index).zfill(digits) + '.' + keep.group(2).lower()
-            self.files[f] = newname
+            if len(self.base_directory[f]) > 0:
+                oldname = self.base_directory[f] + '/' + f
+                newname = self.base_directory[f] + '/' + newname
+            self.files[oldname] = newname
             index -= 1
         return index
 
 
-
-    def print_changes(self):
-        for f in sorted(self.files.keys()):
-            if f == self.files[f]:
-                print '{}'.format(f)
+    def update_files(self, files=None):
+        for f in sorted(files.keys()):
+            if f == files[f]:
+                sys.stderr.write('{}\n'.format(f))
             else:
-                print '{} -> {}'.format(f, self.files[f])
-
-
-
-    def update_files(self):
-        for f in sorted(self.files.keys()):
-            os.rename(f, self.files[f])
+                sys.stderr.write('{} -> {}\n'.format(f, self.files[f]))
+                if not self.noop: os.rename(f, files[f])
 
 
 
@@ -87,20 +96,14 @@ def main():
         sys.exit(1)
     s = NumberPics(debug=args.debug, noop=args.noop, prepend=args.prepend, nameformat=args.nameformat)
     s.build_list(filelist=args.src)
-
-    total_files = len(s.filenames_to_change) + len(s.filenames_already_matching)
-    num_digits = len(str(total_files))
-    # Begin renaming files from the last.
+    # Begin renaming files from the last and working our way toward the first.
     if args.prepend:
-        last_index = s.replace_files(files=s.filenames_already_matching, index=total_files, digits=num_digits)
+        last_index = s.replace_files(files=s.filenames_already_matching, index=s.total_files, digits=s.num_digits)
     else:
-        last_index = total_files
-    last_index = s.replace_files(files=s.filenames_to_change, replace_with=s.nameformat, index=last_index, digits=num_digits)
-    last_index = s.replace_files(files=s.filenames_already_matching, index=last_index, digits=num_digits)
-
-    if s.debug: s.print_changes()
-
-    if not s.noop: s.update_files()
+        last_index = s.total_files
+    last_index = s.replace_files(files=s.filenames_to_change, replace_with=s.nameformat, index=last_index, digits=s.num_digits)
+    last_index = s.replace_files(files=s.filenames_already_matching, index=last_index, digits=s.num_digits)
+    s.update_files(files=s.files)
 
 if __name__ == "__main__":
     main()
