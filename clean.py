34e681dde8232eba0c8f91c4e7d03146841f2a5b
me <marty@tipsy.local>
2016-08-08 17:09:04 -0700

    Corrected typo.

--- clean.py
+++ clean.py
@@ -53,7 +53,7 @@ for f in files_to_fix:
         oldname = path[f] + '/' + f
         newname = path[f] + '/' + new_names[f]
     if f != new_names[f]:
-        os.stderr.write('"{}" -> "{}"'.format(oldname, newname))
+        sys.stderr.write('"{}" -> "{}"'.format(oldname, newname))
         if not args.noop: os.rename(oldname, newname)
     if not args.noop and os.path.isfile(newname):
         os.chmod(newname, 0644)
